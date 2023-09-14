3ccf47e316d622760eef6c648f4bc6c812303619
NatLane2002 <nathaniel.lane@keyin.com>
2023-09-14 17:29:30 -0230

    Final Commit

--- TestTime.java
+++ TestTime.java
@@ -1,3 +1,23 @@
 public class TestTime {
-    
+
+  public static void main(String[] args) {
+    // Create two instances of the Time object (t1 & t2)
+    Time t1 = new Time();
+    Time t2 = new Time();
+
+    // Set t1's and t2's attributes
+    t1.setTime(21, 10, 15);
+    t2.setTime(10, 20, 25);
+
+    // Increment t1's seconds by 1 and decrement t2's seconds by 1
+    t1.nextSecond();
+    t2.previousSecond();
+
+    System.out.println("\n");
+
+    // Display the time for t1 and t2
+    System.out.println("Time 1: " + t1.toString());
+    System.out.println("Time 2: " + t2.toString());
+
+  }
 }
--- Time.java
+++ Time.java
@@ -1,31 +1,81 @@
 public class Time {
-    // Attributes
 
-    private int hour = 14;
-    private int minute = 34;
-    private int second = 26;
+  // Attributes
 
-    // Constructors
+  private int hour = 14;
+  private int minute = 34;
+  private int second = 26;
 
-    // Parameterized Constructor
-    public Time(int h, int m, int s) {
-        this.hour = h;
-        this.minute = m;
-        this.second = s;
-    }
+  // Constructors
 
-    // Methods
+  // Argumentless Constructor (it was giving me an error without this here)
+  public Time() {
+    this.hour = 14;
+    this.minute = 34;
+    this.second = 26;
+  }
 
-    // Gets the hour
-    public int getHour() {
-        return this.hour;
-    }
+  // Parameterized Constructor
+  public Time(int h, int m, int s) {
+    this.hour = h;
+    this.minute = m;
+    this.second = s;
+  }
 
-    // Gets the minute
-    public int getMinute() {
-        return this.minute;
-    }
+  // Methods
 
-    // Gets the second
+  // Gets the hour
+  public int getHour() {
+    return this.hour;
+  }
+
+  // Gets the minute
+  public int getMinute() {
+    return this.minute;
+  }
+
+  // Gets the second
+  public int getSecond() {
+    return this.second;
+  }
+
+  // Sets the hour
+  public void setHour(int hour) {
+    this.hour = hour;
+  }
+
+  // Sets the minute
+  public void setMinute(int minute) {
+    this.minute = minute;
+  }
+
+  // Sets the second
+  public void setSecond(int second) {
+    this.second = second;
+  }
+
+  // Sets every attribute for the Time object
+  public void setTime(int hour, int minute, int second) {
+    this.hour = hour;
+    this.minute = minute;
+    this.second = second;
+  }
+
+  // Formats the time with leading zeros and displays it
+  public String toString() {
+    return String.format("%02d:%02d:%02d", this.hour, this.minute, this.second);
+  }
+
+  // Increments the second attribute for the object instance by 1
+  public Time nextSecond() {
+    this.second += 1;
+    return this;
+  }
+
+  // Decrements the second attribute for the object instance by 1
+  public Time previousSecond() {
+    this.second -= 1;
+    return this;
+  }
 
 }
